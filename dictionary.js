8b8fe92041ee79b812920489a7ca8ee1b9feb6fd
Fearless md <167037126+Whatappl@users.noreply.github.com>
2025-10-13 20:10:34 +0100

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,7 @@
+{
+  "apple": "A fruit that is usually red, green, or yellow.",
+  "book": "A set of written or printed pages bound together.",
+  "computer": "An electronic device for storing and processing data.",
+  "python": "A high-level programming language for general-purpose programming.",
+  "school": "An institution for educating children or students."
+}
