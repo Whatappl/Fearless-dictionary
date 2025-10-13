fee8effe578f8797b30b7bd3704522a664574603
Fearless md <167037126+Whatappl@users.noreply.github.com>
2025-10-13 20:52:23 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+‎const fs = require('fs');
+‎const cors = require('cors');
+‎
+‎const app = express();
+‎const PORT = 3000;
+‎
+‎app.use(cors());
+‎app.use(express.json());
+‎
+‎let dictionary = {};
+‎fs.readFile('dictionary.json', 'utf8', (err, data) => {
+‎  if (err) console.error('Error reading dictionary.json:', err);
+‎  else {
+‎    dictionary = JSON.parse(data);
+‎    console.log('Dictionary loaded successfully!');
+‎  }
+‎});
+‎
+‎app.get('/search', (req, res) => {
+‎  const word = req.query.word?.toLowerCase();
+‎  if (!word) return res.json({ definition: "Please type a word!" });
+‎
+‎  const definition = dictionary[word];
+‎  res.json({ definition: definition ? definition : "Word not found!" });
+‎});
